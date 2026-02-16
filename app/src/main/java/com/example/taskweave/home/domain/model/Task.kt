44860cdb15e5170c3d3417f33a44c3708e9c5723
Rishav Agrawal <workmode0703@gmail.com>
2026-02-17 00:21:00 +0530

    feat/task-screen: add-domain-model-repository

---
+++ app/src/main/java/com/example/taskweave/home/domain/model/Task.kt
@@ -0,0 +1,7 @@
+package com.example.taskweave.home.domain.model
+
+data class Task(
+    val id: String = "",
+    val name: String = "",
+    val completed: Boolean = false,
+)
--- app/src/main/java/com/example/taskweave/home/domain/repository/ProjectRepository.kt
+++ app/src/main/java/com/example/taskweave/home/domain/repository/ProjectRepository.kt
@@ -1,22 +1,23 @@
 package com.example.taskweave.home.domain.repository
 
 import com.example.taskweave.home.domain.model.Project
+import com.example.taskweave.home.domain.model.Task
 import kotlinx.coroutines.flow.Flow
 
 interface ProjectRepository {
     fun observeProjects(): Flow<List<Project>>
     fun observeProject(projectId: String): Flow<Project>
-//    fun observeTasks(projectId: String): Flow<List<Task>>
+    fun observeTasks(projectId: String): Flow<List<Task>>
 
     suspend fun createProject(name: String)
     suspend fun deleteProject(projectId: String)
     suspend fun joinProject(projectId: String)
 
 
-//    suspend fun createTask(projectId: String, name: String)
-//    suspend fun deleteTask(projectId: String, taskId: String)
-//    suspend fun renameTask(projectId: String, taskId: String, newName: String)
-//    suspend fun toggleTask(projectId: String, taskId: String)
+    suspend fun createTask(projectId: String, name: String)
+    suspend fun deleteTask(projectId: String, taskId: String)
+    suspend fun renameTask(projectId: String, taskId: String, newName: String)
+    suspend fun toggleTask(projectId: String, taskId: String)
 
 
 }
